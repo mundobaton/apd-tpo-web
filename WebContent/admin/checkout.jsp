91545892bf2fac2a68467d2eae6f72c42dfc8299
Erika Nuñez <erikannunez@gmail.com>
2018-07-01 01:03:50 -0300

    checkout

--- WebContent/admin/checkout.jsp
+++ WebContent/admin/checkout.jsp
@@ -5,49 +5,27 @@
 
 <section id="checkout">
 	<h2 class="title">Su pedido</h2>
-	<div class="row">
-		<div class="col-md-4 order-md-2 mb-4">
-			<h4 class="d-flex justify-content-between align-items-center mb-3">
-				<span class="text-muted">Su carrito</span> <span
-					class="badge badge-secondary badge-pill">3</span>
-			</h4>
-			<ul class="list-group mb-3">
-				<li
-					class="list-group-item d-flex justify-content-between lh-condensed">
-					<div>
-						<h6 class="my-0">Articulo 1</h6>
-						<small class="text-muted">Cantidad: <span class="quantity">10</span></small>
-					</div> <span class="text-muted">$12</span>
-				</li>
-				<li
-					class="list-group-item d-flex justify-content-between lh-condensed">
-					<div>
-						<h6 class="my-0">Articulo 2</h6>
-						<small class="text-muted">Cantidad: <span class="quantity">10</span></small>
-					</div> <span class="text-muted">$8</span>
-				</li>
-				<li
-					class="list-group-item d-flex justify-content-between lh-condensed">
-					<div>
-						<h6 class="my-0">Articulo 3</h6>
-						<small class="text-muted">Cantidad: <span class="quantity">10</span></small>
-					</div> <span class="text-muted">$5</span>
-				</li>
-				<li class="list-group-item d-flex justify-content-between"><span>Total
-						(AR$)</span> <strong>$20</strong></li>
-			</ul>
+	<form id="checkout-form" method="post" enctype="multipart/form-data" action="?" class="needs-validation" novalidate="">
+		<div class="row">
+			<div class="col-md-4 order-md-2 mb-4">
+				<h4 class="d-flex justify-content-between align-items-center mb-3">
+					<span class="text-muted">Su carrito</span> <span id="cantTotalItems"
+						class="badge badge-secondary badge-pill"></span>
+				</h4>
+				<ul id="mini-cart" class="list-group mb-3">
+				</ul>
 
-		</div>
-		<div class="col-md-8 order-md-1">
-			<h4 class="mb-3">Dirección de enví­o</h4>
-			<form class="needs-validation" novalidate="">
+			</div>
+			<div class="col-md-8 order-md-1">
+				<h4 class="mb-3">Dirección de enví­o</h4>
+
 
 				<div class="mb-3">
 					<label for="address">Calle y número</label> <input type="text"
 						class="form-control" id="address"
 						placeholder="Calle Principal 123" required="">
-					<div class="invalid-feedback">Por favor, indique su
-						dirección de enví­o.</div>
+					<div class="invalid-feedback">Por favor, indique su dirección
+						de enví­o.</div>
 				</div>
 
 				<div class="mb-3">
@@ -78,16 +56,16 @@
 					<div class="col-md-3 mb-3">
 						<label for="zip">Código Postal</label> <input type="text"
 							class="form-control" id="zip" placeholder="" required="">
-						<div class="invalid-feedback">El código postal es
-							requerido.</div>
+						<div class="invalid-feedback">El código postal es requerido.</div>
 					</div>
 				</div>
 				<hr class="mb-4">
 				<button class="btn btn-primary btn-lg btn-block" type="submit">Confirmar
 					pedido</button>
-			</form>
+
+			</div>
 		</div>
-	</div>
+	</form>
 </section>
 
 </main>
--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -12,7 +12,9 @@ $(function() {
 				$('#addToCart p').html(
 						btn.parents(".article").find(".description").html());
 				$('#aid').val(btn.data('aid'));
-				$('#precio').val(btn.parents(".article").find(".price-tag > span").text());
+				$('#precio').val(
+						btn.parents(".article").find(".price-tag > span")
+								.text());
 			});
 
 	$('#addToCart').on('hide.bs.modal', function(e) {
@@ -26,9 +28,9 @@ $(function() {
 		e.preventDefault();
 		var item = {
 			aid : parseInt($('input[name=aid]').val()),
-			nombre: $('#addToCart h3').text(),
+			nombre : $('#addToCart h3').text(),
 			cant : parseInt($('input[name=cantidad]').val()),
-			precio: parseFloat($('input[name=precio]').val())
+			precio : parseFloat($('input[name=precio]').val())
 		}
 		addItemToCart(item);
 		$('#addToCart').modal('hide');
@@ -50,28 +52,72 @@ $(function() {
 		var $this = $(e.target);
 		// TODO Actualizar el total del carrito cuando se elimina un elemento.-
 		$('#cart').find("#aid-" + $this.data('aid')).remove();
+		removeItemFromCart(parseInt($this.data('aid')));
 		$('#removeFromCart').modal('hide');
 	});
-	
-	if($('#cart').length > 0 ){
+
+	if ($('#cart').length > 0) {
 		if (typeof (Storage) !== "undefined") {
 			if (sessionStorage.dvlCart) {
 				var $cart = JSON.parse(sessionStorage.dvlCart);
 				var $table = $('#cart .table tbody');
 				var subtotal = 0;
-				for (var i = 0; i < $cart.length; i++) {
-					var fila = '<tr class="cart-item" id="aid-'+$cart[i].id+'">';
-					fila += '<td class="item-title">'+$cart[i].nombre+'</td>';
-					fila += '<td class="item-price text-center">$'+$cart[i].precio.toFixed(2)+'</td>';
-					fila += '<td class="item-quantity text-center">'+$cart[i].cant+'</td>';
 
-//					fila += '<td class="item-quantity text-center"><input type="number" step="1" min="1" value="'+$cart[i].cant+'" class="form-control" /></td>';
-					fila += '<td class="item-subtotal text-center">$'+($cart[i].precio * $cart[i].cant).toFixed(2)+'</td>';
-					fila += '<td class="text-center"><a class="item-remove" href="#" data-aid="1" data-toggle="modal" data-target="#removeFromCart"><i class="fas fa-trash-alt"></i></a></td></tr>';
-					subtotal += ($cart[i].precio * $cart[i].cant);
-					$table.append(fila);
+				for (var i = 0; i < $cart.length; i++) {
+					if ($cart[i]) {
+						var fila = '<tr class="cart-item" id="aid-'
+								+ $cart[i].aid + '">';
+						fila += '<td class="item-title">' + $cart[i].nombre
+								+ '</td>';
+						fila += '<td class="item-price text-center">$'
+								+ $cart[i].precio.toFixed(2) + '</td>';
+						fila += '<td class="item-quantity text-center">'
+								+ $cart[i].cant + '</td>';
+						// fila += '<td class="item-quantity text-center"><input
+						// type="number" step="1" min="1"
+						// value="'+$cart[i].cant+'"
+						// class="form-control" /></td>';
+						fila += '<td class="item-subtotal text-center">$'
+								+ ($cart[i].precio * $cart[i].cant).toFixed(2)
+								+ '</td>';
+						fila += '<td class="text-center"><a class="item-remove" href="#" data-aid="'
+								+ $cart[i].aid
+								+ '" data-toggle="modal" data-target="#removeFromCart"><i class="fas fa-trash-alt"></i></a></td></tr>';
+						subtotal += ($cart[i].precio * $cart[i].cant);
+						$table.append(fila);
+					}
 				}
 				$('#cart-subtotal').text(subtotal.toFixed(2));
+			}
+		}
+	}
+
+	if ($('#checkout').length > 0) {
+		if (typeof (Storage) !== "undefined") {
+			if (sessionStorage.dvlCart) {
+				var $cart = JSON.parse(sessionStorage.dvlCart);
+				var cantItems = 0;
+				var subtotal = 0;
+				var miniCart = $('#mini-cart');
+				for (var i = 0; i < $cart.length; i++) {
+					if ($cart[i]) {
+						cantItems++;
+						var fila = '<li class="list-group-item d-flex justify-content-between lh-condensed">';
+						fila += '<div><h6 class="my-0">'+$cart[i].nombre+'</h6>';
+						fila += '<small class="text-muted">Cantidad: <span class="quantity">'+$cart[i].cant+'</span></small>';
+						fila += '</div> <span class="text-muted">'+$cart[i].precio.toFixed(2)+'</span>';
+						fila += '<input type="hidden" name="articulos[]" value="'+$cart[i].aid+','+$cart[i].cant+'"/>';					
+						fila += '</li>';
+						subtotal += ($cart[i].precio * $cart[i].cant);
+						miniCart.append(fila);
+					}
+				}
+				if(cantItems != 0){
+					$('#cantTotalItems').text(cantItems);
+				}
+				if(subtotal != 0){
+					miniCart.append($('<li class="list-group-item d-flex justify-content-between"><span>Total (AR$)</span> <strong>$'+subtotal.toFixed(2)+'</strong></li>'));
+				}				
 			}
 		}
 	}
@@ -85,9 +131,12 @@ function addItemToCart(cartItem) {
 			var oldCart = JSON.parse(sessionStorage.dvlCart);
 			var newItem = true;
 			for (var i = 0; i < oldCart.length; i++) {
-				if (oldCart[i].aid == cartItem.aid) {
-					newItem = false;
-					oldCart[i].cant = parseInt(oldCart[i].cant) + parseInt(cartItem.cant);
+				if (oldCart[i]) {
+					if (oldCart[i].aid == cartItem.aid) {
+						newItem = false;
+						oldCart[i].cant = parseInt(oldCart[i].cant)
+								+ parseInt(cartItem.cant);
+					}
 				}
 			}
 			if (newItem) {
@@ -99,44 +148,14 @@ function addItemToCart(cartItem) {
 			items.push(cartItem);
 			sessionStorage.setItem("dvlCart", JSON.stringify(items));
 		}
-		showAlert('Su carrito ha sido actualizado! <a href="'+getBaseUrl()+'/admin/cart.jsp">Ver carrito</a>');
+		showAlert('Su carrito ha sido actualizado! <a href="' + getBaseUrl()
+				+ '/admin/cart.jsp">Ver carrito</a>');
 		var carrito = JSON.parse(sessionStorage.dvlCart);
 		console.log(carrito);
 
 	} else {
 		console.log("Sin soporte para session storage");
 	}
-}
-
-function updateCartItem(cartItem) {
-
-	if (typeof (Storage) !== "undefined") {
-		if (sessionStorage.dvlCart) {
-			var oldCart = JSON.parse(sessionStorage.dvlCart);
-			for (var i = 0; i < oldCart.length; i++) {
-				if (oldCart[i].aid == cartItem.aid) {
-					oldCart[i].cant = parseInt(cartItem.cant);
-				}
-			}
-			sessionStorage.setItem("dvlCart", JSON.stringify(oldCart));
-		}
-		var carrito = JSON.parse(sessionStorage.dvlCart);
-		console.log(carrito);
-
-	} else {
-		console.log("Sin soporte para session storage");
-	}
-}
-
-function showAlert(message){
-	var alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">';
-	alert += '<span class="alert-message">'+message+'</span>';
-	alert += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
-	alert += '<span aria-hidden="true">&times;</span>';
-	alert += '</button></div>';
-	
-	$(alert).insertBefore('section > .title');
-	
 }
 
 function removeItemFromCart(articleId) {
@@ -144,12 +163,18 @@ function removeItemFromCart(articleId) {
 	if (typeof (Storage) !== "undefined") {
 		if (sessionStorage.dvlCart) {
 			var oldCart = JSON.parse(sessionStorage.dvlCart);
+			var subtotal = 0;
 			for (var i = 0; i < oldCart.length; i++) {
-				if (oldCart[i].aid == articleId) {
-					oldCart[i].cant = 0;
+				if (oldCart[i]) {
+					if (oldCart[i].aid == articleId) {
+						delete oldCart[i];
+					} else {
+						subtotal += (oldCart[i].precio * oldCart[i].cant);
+					}
 				}
 			}
 			sessionStorage.setItem("dvlCart", JSON.stringify(oldCart));
+			$('#cart-subtotal').text(subtotal.toFixed(2));
 		}
 
 		var carrito = JSON.parse(sessionStorage.dvlCart);
@@ -158,6 +183,17 @@ function removeItemFromCart(articleId) {
 	} else {
 		console.log("Sin soporte para session storage");
 	}
+}
+
+function showAlert(message) {
+	var alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">';
+	alert += '<span class="alert-message">' + message + '</span>';
+	alert += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+	alert += '<span aria-hidden="true">&times;</span>';
+	alert += '</button></div>';
+
+	$(alert).insertBefore('section > .title');
+
 }
 
 function getBaseUrl() {
