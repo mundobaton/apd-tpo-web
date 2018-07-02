bbebebd58a1e9d7a6fe7472d5a3e96a9a4b2a0c8
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2018-07-01 23:10:47 -0300

    facturacion servlet

---
+++ src/main/java/edu/uade/apd/tpo/web/servlet/FacturacionServlet.java
@@ -0,0 +1,45 @@
+package edu.uade.apd.tpo.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.uade.apd.tpo.repository.delegate.FacturacionDelegate;
+
+public class FacturacionServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2561960437404457527L;
+	private FacturacionDelegate delegate;
+	private static final String ACTION = "action";
+	private static final String FACTURAR = "facturar";
+
+	@Override
+	public void init() throws ServletException {
+		try {
+			delegate = FacturacionDelegate.getInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		super.init();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String action = req.getParameter(ACTION);
+		if (action.equals(FACTURAR)) {
+			Long pedidoId = Long.parseLong(req.getParameter("pid"));
+			delegate.facturar(pedidoId);
+			navigate(req, resp, "/facturacion/pedido.jsp?pedidoId=" + pedidoId + "&result=success");
+		}
+	}
+
+	private void navigate(HttpServletRequest req, HttpServletResponse resp, String url)
+			throws IOException, ServletException {
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+		dispatcher.forward(req, resp);
+	}
+}
